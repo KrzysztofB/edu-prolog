a77d80e510b74206f7fec1429c4b54b90e8d4f5c
Krzysztof Bardzinski <krzysztof.bardzinski@wp.pl>
2024-09-03 22:48:16 +0200

    anagram with comments

--- exercism.org/anagram.pl
+++ exercism.org/anagram.pl
@@ -1,14 +1,19 @@
-anagram(Word, [Word], [Word]).
+
+%% find anagrams:
+%% - ignore case
+%% - same word is not an anagram
+%% - remove duplicates
+%% anagram(InputWord, AnagramCandidates, ResultAnagrams)
 anagram(Word, Candidates, Anagrams):- 
-    string_length(Word, WordLen),
-    string_lower(Word, WordLower),
-    string_chars(WordLower, WordLetters),
-    msort(WordLetters, WordLettersSorted),
+    string_length(Word, WordLen),           % precalculation of length
+    string_lower(Word, WordLower),          
+    string_chars(WordLower, WordLetters),   
+    msort(WordLetters, WordLettersSorted),  % sorted letters in word lowercased
 	findall(X,(
-		member(X, Candidates), 
-		string_length(X, WordLen),
-        string_lower(X, XLower),
-        WordLower \== XLower,
+		member(X, Candidates),              % get next candidate X
+		string_length(X, WordLen),          % candidate length must match word length
+        string_lower(X, XLower),            
+        WordLower \== XLower,               % make sure its not the same word ignoring case
         string_chars(XLower, XLetters),
-        msort(XLetters, WordLettersSorted)
+        msort(XLetters, WordLettersSorted)  % same sorted list of letters, it's an anagram
 		), Anagrams).
