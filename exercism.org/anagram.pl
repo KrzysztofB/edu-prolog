789a27a65a5e85089fe3b4ca33e565bc87cc26da
Krzysztof Bardzinski <krzysztof.bardzinski@wp.pl>
2024-09-03 22:52:56 +0200

    anagram comments updated

--- exercism.org/anagram.pl
+++ exercism.org/anagram.pl
@@ -2,18 +2,17 @@
 %% find anagrams:
 %% - ignore case
 %% - same word is not an anagram
-%% - remove duplicates
 %% anagram(InputWord, AnagramCandidates, ResultAnagrams)
 anagram(Word, Candidates, Anagrams):- 
     string_length(Word, WordLen),           % precalculation of length
     string_lower(Word, WordLower),          
     string_chars(WordLower, WordLetters),   
-    msort(WordLetters, WordLettersSorted),  % sorted letters in word lowercased
+    msort(WordLetters, WordLettersSorted),  % sorted letters of lowercased word
 	findall(X,(
 		member(X, Candidates),              % get next candidate X
 		string_length(X, WordLen),          % candidate length must match word length
         string_lower(X, XLower),            
         WordLower \== XLower,               % make sure its not the same word ignoring case
         string_chars(XLower, XLetters),
-        msort(XLetters, WordLettersSorted)  % same sorted list of letters, it's an anagram
+        msort(XLetters, WordLettersSorted)  % same sorted list of letters, so X is an anagram
 		), Anagrams).
