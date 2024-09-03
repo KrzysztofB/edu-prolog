8085bb52c8473de857716b5bbf10af88544985bb
Krzysztof Bardzinski <krzysztof.bardzinski@wp.pl>
2024-09-03 22:57:05 +0200

    tabs to spaces

--- exercism.org/anagram.pl
+++ exercism.org/anagram.pl
@@ -8,11 +8,11 @@ anagram(Word, Candidates, Anagrams):-
     string_lower(Word, WordLower),          
     string_chars(WordLower, WordLetters),   
     msort(WordLetters, WordLettersSorted),  % sorted letters of lowercased word
-	findall(X,(
-		member(X, Candidates),              % get next candidate X
-		string_length(X, WordLen),          % candidate length must match word length
+    findall(X,(
+        member(X, Candidates),              % get next candidate X
+        string_length(X, WordLen),          % candidate length must match word length
         string_lower(X, XLower),            
         WordLower \== XLower,               % make sure its not the same word ignoring case
         string_chars(XLower, XLetters),
         msort(XLetters, WordLettersSorted)  % same sorted list of letters, so X is an anagram
-		), Anagrams).
+    ), Anagrams).
